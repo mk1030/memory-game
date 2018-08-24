e0fffe3c895624e3738735158e83d80e8f7789dc
mk203 <mk203@duke.edu>
2018-08-24 15:08:33 -0400

    Working on getting card symbols to hide/show

--- js/app.js
+++ js/app.js
@@ -2,6 +2,55 @@
  * Create a list that holds all of your cards
  */
 
+ $( document ).ready(function() {
+
+
+
+
+   // Shuffle function from http://stackoverflow.com/a/2450976
+   function shuffle(array) {
+       var currentIndex = array.length, temporaryValue, randomIndex;
+
+       while (currentIndex !== 0) {
+           randomIndex = Math.floor(Math.random() * currentIndex);
+           currentIndex -= 1;
+           temporaryValue = array[currentIndex];
+           array[currentIndex] = array[randomIndex];
+           array[randomIndex] = temporaryValue;
+       }
+
+       return array;
+   }
+
+
+let cards = $(".cards").children();
+
+cards = $.makeArray( cards );
+
+for (var i = 0; i < cards.length; i++){
+  $( cards[i]).click(function() {
+   $( this).toggleClass( "open" ).find("i").show();
+});
+
+};
+
+
+
+
+
+
+
+
+
+
+
+shuffle(cards);
+
+
+
+// var icards = cards[x].getElementsByTagName("i");
+
+});
 
 /*
  * Display the cards on the page
@@ -9,22 +58,9 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
+// for (var x = 0; x < cards.length; x++){
+// }
+// .find( "i" ).toggleClass("fa-diamond")
 
 /*
  * set up the event listener for a card. If a card is clicked:
