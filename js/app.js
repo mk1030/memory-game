7d4f4a3dfc234fc26c0a17ea718f3d0a3463f332
Madeeha Khan <madeeha1030@gmail.com>
2018-08-25 19:12:36 -0400

    Shuffle function and Start Game function working

--- js/app.js
+++ js/app.js
@@ -5,8 +5,6 @@
  $( document ).ready(function() {
 
 
-
-
    // Shuffle function from http://stackoverflow.com/a/2450976
    function shuffle(array) {
        var currentIndex = array.length, temporaryValue, randomIndex;
@@ -22,11 +20,17 @@
        return array;
    }
 
-
+//grab the cards this returns an object
 let cards = $(".cards").children();
 
+
+//convert the object into an array, so we can use the built-in methods for array
 cards = $.makeArray( cards );
 
+
+//loop through the cards array. 
+//Add a click function to each card that will toggle to "open" the card and will disable it. 
+//The i element is initially hidden with CSS, but add a toggle that will make it visible when clicked and invisible when clicked again. 
 for (var i = 0; i < cards.length; i++){
   $( cards[i]).click(function() {
    $( this).toggleClass( "open disabled" ).find("i").toggle();
@@ -37,20 +41,27 @@ for (var i = 0; i < cards.length; i++){
 
 
 
+//get the entire ul html elements
+const deck = $(".cards").get(0);
+console.log(deck);
 
-const deck = document.querySelector(".cards");
+
 function startGame(){
+	//get an array of shuffled cards 
    var shuffledCards = shuffle(cards);
-   for (var i= 0; i < shuffledCards.length; i++){
-      [].forEach.call(shuffledCards, function(item){
-         deck.appendChild(item);
-      });
-   }
+  console.log(shuffledCards);
+	  //loop through this shuffled card array. In an each loop you must pass the index and value. 
+	  //run a function at each index, that will add the value of each index (the li.card) to the deck element. 
+	   $.each(shuffledCards, function(item, value) {
+		$(deck).append(value)
+		  
+	  }); 
+   
 }
 
-window.onload = startGame();
+startGame();
 
-// var icards = cards[x].getElementsByTagName("i");
+
 
 });
 
@@ -73,4 +84,11 @@ window.onload = startGame();
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ 
+  for (var i= 0; i < shuffledCards.length; i++){
+      [].forEach.call(shuffledCards, function(item){
+         deck.appendChild(item);
+      });
+   }
+ 
  */
